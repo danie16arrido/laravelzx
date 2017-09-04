b43d258d17c8a225ecb643d62c0e8b4db220dbd6
danie1garrido <daniel.garrido.info@gmail.com>
2017-09-04 11:57:11 +0100

    add controllers

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,10 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <ul>
-        @foreach ($tasks as $task)
-            <li>{{ $task->body }}</li>
-        @endforeach
-    </ul>
+ 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,8 @@
 |
 */
 
-Route::get('/tasks', function () {
-    // $tasks = DB::table('tasks')->get();
-    $tasks = App\Task::all();
-    return view('tasks.index', compact('tasks'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
 
-Route::get('/tasks/{task}', function ($id) {
-
-    // $task = DB::table('tasks')->find($id);
-    $task = App\Task::find($id);
-    return view('tasks.show', compact('task'));
-});
 
  
