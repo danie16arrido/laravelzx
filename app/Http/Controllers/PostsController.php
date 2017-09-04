569cbd68ec4d197df20ab57791e606b69877743b
danie1garrido <daniel.garrido.info@gmail.com>
2017-09-04 16:44:04 +0100

    add form of type post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 class PostsController extends Controller
 {
     public function index(){
@@ -12,5 +12,26 @@ class PostsController extends Controller
 
     public function show(){
         return view('posts.show');
+    }
+
+    public function create(){
+        return view('posts.create');
+    }
+
+    public function store(){
+        //create a new post using the request data
+        // dd(request(['title', 'body']));
+        // $post = new Post;
+        // $post->title = request('title');
+        // $post->body = request('body');
+        // //save it to to the DB
+        // $post->save();
+        Post::create([
+            'title' => request('title'),
+            'body' => request('body')
+        ]);
+        //redirect  
+        return redirect('/');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,12 @@
 |
 */
 
-Route::get('/tasks', 'TasksController@index');
-Route::get('/tasks/{task}', 'TasksController@show');
+// Route::get('/tasks', 'TasksController@index');
+// Route::get('/tasks/{task}', 'TasksController@show');
 Route::get('/', 'PostsController@index');
-Route::get('/posts/{post}', 'PostsController@show');
+// Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/create', 'PostsController@create');
+Route::post('/post', 'PostsController@store');
 
 
  
